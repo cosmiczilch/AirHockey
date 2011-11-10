c4110520df17f9e648a33eb36c78d9771f75c4ec
cosmiczilch <cosmiczilch@gmail.com>
2011-11-10 19:03:33 +0530

    scripting...

--- game_running.h
+++ game_running.h
@@ -517,41 +517,68 @@ void scripting_for_player2( ) {
 
 	me->motion.velocity[VY] = 0.0;
 
-	if (puck.motion.velocity[VY] > 0) {	/* puck is moving toward 'my' goalPost 		*/
+	if (puck.motion.velocity[VY] > 0) {	/* puck is moving toward 'my' goalPost 	*/
 
 		/**
 		 * find the time the puck would take to arrive (in ticks)
 		 * at my 'y' if it were to continue along at its current velocity.
 		 * is this overcounting? yes (due to friction)
 		 */
-		float time_left = 	(me->y - puck.y) /\
-					puck.motion.velocity[VY];
-		/**
-		 * we should try and intercept the puck at the x it would be at
-		 * time_left ticks from now; so, calculate the velocity[VX] at
-		 * which we'll have to sidle across
-		 */
-		float delta_x = puck.x - me->x;
-		me->motion.velocity[VX] = MAX_PUCK_SPEED_SUSP * delta_x / BOARD_WIDTH;
+		float time_left = abs( (me->y - puck.y) /\
+			   	  puck.motion.velocity[VY]);
 
 		/**
-		 * if there is too much time_left, better orient myself to
-		 * score a goal along w/ just defending my goalPost
-		 * this should be a function of AMBITION
+		 * compute a wanna_be_x for us to be at at the end of time_left
 		 */
+		float wanna_be_x;
+		if (time_left > 3 && 	/* lots of time left	 */
+		    puck.y < me->y) {	/* puck is not behind me */
+			/**
+			 * if there is too much time_left, better orient myself to
+			 * score a goal along w/ just defending my goalPost
+			 * this should be a function of AMBITION
+			 * i want to make sure that me, the puck and the other goalPost
+			 * are collinear
+			 */
+			float theta = atan2( puck.y - (-BOARD_LENGTH/2.0), puck.x - 0.0 );
+			wanna_be_x = puck.x + cos(theta) * (BAT_RADIUS+PUCK_RADIUS);
+		} else {		/* not too much time left */
+		 	/**
+			 * we should try to at least intercept the puck at the
+			 * x it would be at at the end of time_left
+			 * or, simpler still, we could just try to mimick the puck's x
+			 * at every point till then
+			 */
+			wanna_be_x = puck.x;
+			/**
+			 * if there is not too much time_left, i might want to consider
+			 * (depending on my AMBITION) moving back a little to better 
+			 * align myself to score a goal
+			 */
+		}
 		/**
-		 * if there is not too much time_left, i might want to consider
-		 * (depending on my AMBITION) moving back a little to better 
-		 * align myself to score a goal
+		 * now calculate the velocity[VX] at which we'll have to sidle across
+		 * so that we can be at wanna_be_x at the end of time_left
 		 */
+		float delta_x = wanna_be_x - me->x;
+		me->motion.velocity[VX] = MAX_PUCK_SPEED_SUSP * delta_x / BOARD_WIDTH;
+
+	} else {				/* puck is moving away from 'my' goalPost */
 		/**
 		 * if i am too far back, i might want to come forward a little
 		 * too, towards the middle to better tackle the next attack on
 		 * my goalPost, probably do this in "idle" time (when puck is in
 		 * player1's court and moving away)
 		 */
+		if (puck.y <= 0) { 		/* puck is in player1's court */
+			/**
+			 * try and move my y to the middle of my court.
+			 * note that this is in "idle time"
+			 */
+			float delta_y = BOARD_LENGTH/4.0 - me->y;
+			me->motion.velocity[VY] = delta_y * BOARD_LENGTH/4.0 / MAX_PUCK_SPEED_SUSP;
+		}
 
-	} else {
 		/**
 		 * if the puck is moving away from my goalPost AND its in my court,
 		 * calculate the time_left before it leaves my court, and align
@@ -675,7 +702,7 @@ void init_UI_items( ) {
 	float h = get_GH( ); 
 
 	panels[SCORE_PANEL].init( (int)SCORE_PANEL, w*90/100.0, h*10/100,  0.0, -h*40/100.0, 2*SMALL_EPSILON, \
-	"./resources/images/panel.png" );
+	"./resources/images/score_panel.png" );
 	panels[SCORE_PANEL].visible = true;
 	panels[SCORE_PANEL].enabled = true;
 
